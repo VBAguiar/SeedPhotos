066173d60068e6c7483789019e3cb84a61831dc5
Vinicius Barbosa de Aguiar <vb9763662@gmail.com>
2022-02-01 12:29:59 -0300

    test for photos.py

--- tests/test_seedphotos.py
+++ tests/test_seedphotos.py
@@ -1,5 +1,15 @@
-from seedphotos import __version__
+import unittest
 
+class test_seedphotos(unittest.TestCase):
+    def test_seedphotos(self):
+        photos = Photos(width=300, height=300, filename='image', seed='Vinicius-Barbosa', folder='/home/barbosa/', format='jpg')
+        photosGetPhotos = photos.getPhotos()
 
-def test_version():
-    assert __version__ == '0.1.0'
+        self.assertEqual(photosGetPhotos, 'OK')
+
+if __name__ == '__main__':
+    from sys import path; from pathlib import Path
+    path.insert(0, str(Path(__file__).resolve().parent.parent))
+    from seedphotos.photos import Photos
+    unittest.main()
+    
